0c6d77514cfb029c466769ad597fcbf90b84460e
Hoodoo42 <create@hoodooverse.ca>
2022-07-28 22:38:20 -0600

    restructured the code to have all functions on top, then the requests, then the variable sets and commented

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
+// PART ONE - Method POST - Function
 // the eventlistener function houses the axios request, and the values that are going to be entered by the user.
-
 function postPost(details) {
   let postTitleValue = document.getElementById(`post_title`)[`value`];
   let userValue = document.getElementById(`user_name`)[`value`];
@@ -18,6 +18,7 @@ function postPost(details) {
     .then(postSuccess)
     .catch(postFailure);
 }
+// PART ONE - Method POST success/failure Functions
 // the success function will run a success message if request is successful
 function postSuccess(response) {
   let statusMessage = document.getElementById(`status_message`);
@@ -27,50 +28,61 @@ function postSuccess(response) {
 function postFailure(error) {
   statusMessage[`innerHTML`] = `<h3>Error!</h3>`;
 }
-
-// creating an axios request that will run when a button is clicked. This sets the button with an eventlistener
-let submit = document.getElementById(`submit_button`);
-submit.addEventListener(`click`, postPost);
-
-
-function postUpdateSuccess(details){
-    let statusMessage = document.getElementById(`status_message`);
-    statusMessage[`innerHTML`] = `<h3>Your post has been updated!</h3>`;
+// PART TWO - Method PATCH success/failure Functions
+function postUpdateSuccess(details) {
+  let statusMessage = document.getElementById(`status_message`);
+  statusMessage[`innerHTML`] = `<h3>Your post has been updated!</h3>`;
 }
 
-function postUpdateFailure(error){
-    statusMessage[`innerHTML`] = `<h3>Error!</h3>`;
+function postUpdateFailure(error) {
+  statusMessage[`innerHTML`] = `<h3>Error!</h3>`;
 }
-// this axios request is a patch, it will take the new data in title, and update the old post title. 
-axios.request({
+
+// PART THREE - Method DELETE success/failure functions
+function deleteSuccess(response) {
+  document.body.insertAdjacentHTML(
+    `beforeend`,
+    `
+    <h3>your post has been deleted</h3>`
+  );
+}
+
+function deleteFailure(error) {
+  document.body.insertAdjacentHTML(
+    `beforeend`,
+    `
+    <h3>your post has NOT been deleted</h3>`
+  );
+}
+// PART TWO - Method PATCH axios.request
+// this axios request is a patch, it will take the new data in title, and update the old post title.
+axios
+  .request({
     url: `https://jsonplaceholder.typicode.com/posts/1`,
 
     method: `PATCH`,
 
     data: {
-        title: `This will be the new title.`
-    }
+      title: `This will be the new title.`,
+    },
+  })
+  .then(postUpdateSuccess)
+  .catch(postUpdateFailure);
 
 
-}).then(postUpdateSuccess).catch(postUpdateFailure);
-
-
-function deleteSuccess(response){
-  document.body.insertAdjacentHTML(`beforeend`, `
-  <h3>your post has been deleted</h3>`)
-}
-
-
-function deleteFailure(error){
-    document.body.insertAdjacentHTML(`beforeend`, `
-  <h3>your post has NOT been deleted</h3>`)
-}
-
-
+//   PART THREE - Method DELETE axios.request
 // this axios request method delete, deletes the post targeted in the endpoint
-axios.request({
-    url: `https://jsonplaceholder.typicode.com/posts/1`, 
+axios
+  .request({
+    url: `https://jsonplaceholder.typicode.com/posts/1`,
 
     method: `DELETE`,
+  })
+  .then(deleteSuccess)
+  .catch(deleteFailure);
 
-}).then(deleteSuccess).catch(deleteFailure);
+
+//   PART ONE - Method POST button eventListener
+// creating an axios request that will run when a button is clicked. This sets the button with an eventlistener
+let submit = document.getElementById(`submit_button`);
+submit.addEventListener(`click`, postPost);
